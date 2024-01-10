a3aa58694617f102f9a099dacabf82a5cb603794
wood16 <hoanglam1699@gmail.com>
2024-01-10 20:50:38 +0700

    add elasticsearch config

--- src/main/java/com/example/taskmanage/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanage/repository/TaskRepository.java
@@ -3,13 +3,13 @@ package com.example.taskmanage.repository;
 import com.example.taskmanage.entity.TaskEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+public interface TaskRepository extends ElasticsearchRepository<TaskEntity, Long> {
 
     Page<TaskEntity> findAll(Pageable pageable);
 
